90510392e2654b95886284057339be59f8f90cb5
Matthew <cameron.matt@outlook.com>
2021-03-15 10:44:07 +1100

    Done filter lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter( match =>
+      match.toLowerCase() === string.toLowerCase()
+    )
+  }
+
+
+function fuzzyMatch(drivers, letter){
+  return drivers.filter( find => {
+    if (letter.charAt(0) === find.charAt(0)){
+    return find
+    }
+  })
+}
+
+function matchName(drivers) {
+  const newName = drivers.filter(function(driver) {
+    return driver.name == "Bobby";
+  });
+  return newName; 
+}
